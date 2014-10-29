67167afd48b40d4bc2e554fb5bfadc7828ef5d26
dhamaniasad <dhamaniasad@gmail.com>
2014-10-29 10:04:45 +0530

    image types

--- downall.py
+++ downall.py
@@ -6,14 +6,20 @@ import re
 nums = range(1, 1440)
 urls = ["http://xkcd.com/" + str(num) for num in nums]
 for url in urls:
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text)
-    soup2 = soup.find(id="comic")
-    tag = soup2.img
-    type(tag)
-    imgurl = tag['src']
-    imgname = soup.title.string
-    imgname = re.sub(r'[^a-zA-Z0-9 ]', '', imgname)
-    imgname = re.sub('xkcd ', '', imgname)
-    imgfilename = "images/%s.jpg" % imgname
-    getimg = urllib.urlretrieve(imgurl, imgfilename)
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        soup2 = soup.find(id="comic")
+        tag = soup2.img
+        type(tag)
+        imgurl = tag['src']
+        imgname = soup.title.string
+        imgname = re.sub(r'[^a-zA-Z0-9 ]', '', imgname)
+        imgname = re.sub('xkcd ', '', imgname)
+        if imgurl.endswith(('.jpg')):
+            imgfilename = "images/%s.jpg" % imgname
+        else:
+            imgfilename = "images/%s.png" % imgname
+        getimg = urllib.urlretrieve(imgurl, imgfilename)
+    except Exception:
+        print "Failed for", url
