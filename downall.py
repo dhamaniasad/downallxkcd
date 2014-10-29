6b8a10b409770726c349decbe856d3a5642d64f6
dhamaniasad <dhamaniasad@gmail.com>
2014-10-29 06:38:16 +0530

    Added code to get each file and save it to disk

--- downall.py
+++ downall.py
@@ -1,6 +1,16 @@
 import requests
+import urllib
+from bs4 import BeautifulSoup
 
-for x in xrange(1440):
-    r = requests.get("http://xkcd.com/%s" % x)
-    html = r.content
-    print html
+nums = range(1, 1440)
+urls = ["http://xkcd.com/" + str(num) for num in nums]
+for url in urls:
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    soup2 = soup.find(id="comic")
+    tag = soup2.img
+    type(tag)
+    imgurl = tag['src']
+    imgname = soup.title.string
+    imgfilename = "images/%s.jpg" % imgname
+    getimg = urllib.urlretrieve(imgurl, imgfilename)
